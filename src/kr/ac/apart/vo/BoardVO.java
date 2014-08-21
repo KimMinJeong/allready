5005164d90c4cb554da54db1e423895af288d98c
KimMinJeong <sinred12345@naver.com>
2014-08-21 16:58:34 +0900

    spring mybatis 기본 설정

---
+++ src/kr/ac/apart/vo/BoardVO.java
@@ -0,0 +1,98 @@
+package kr.ac.apart.vo;
+
+public class BoardVO {
+	private int board_no;
+	private String writer_id;
+	private String category;
+	private String title;
+	private String contents;
+	private String reg_date;
+	private int view_count;
+	private int good;
+	private int bad;
+	private boolean anonymous;
+	private boolean closed;
+	private String file;
+	@Override
+	public String toString() {
+		return "BoardVO [board_no=" + board_no + ", writer_id=" + writer_id
+				+ ", category=" + category + ", title=" + title + ", contents="
+				+ contents + ", reg_date=" + reg_date + ", view_count="
+				+ view_count + ", good=" + good + ", bad=" + bad
+				+ ", anonymous=" + anonymous + ", closed=" + closed + ", file="
+				+ file + "]";
+	}
+	public int getBoard_no() {
+		return board_no;
+	}
+	public void setBoard_no(int board_no) {
+		this.board_no = board_no;
+	}
+	public String getWriter_id() {
+		return writer_id;
+	}
+	public void setWriter_id(String writer_id) {
+		this.writer_id = writer_id;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+	public int getView_count() {
+		return view_count;
+	}
+	public void setView_count(int view_count) {
+		this.view_count = view_count;
+	}
+	public int getGood() {
+		return good;
+	}
+	public void setGood(int good) {
+		this.good = good;
+	}
+	public int getBad() {
+		return bad;
+	}
+	public void setBad(int bad) {
+		this.bad = bad;
+	}
+	public boolean isAnonymous() {
+		return anonymous;
+	}
+	public void setAnonymous(boolean anonymous) {
+		this.anonymous = anonymous;
+	}
+	public boolean isClosed() {
+		return closed;
+	}
+	public void setClosed(boolean closed) {
+		this.closed = closed;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+	
+}
---
+++ src/kr/ac/apart/vo/ExpressVO.java
@@ -0,0 +1,60 @@
+package kr.ac.apart.vo;
+
+public class ExpressVO {
+	private int express_id;
+	private String user_id;
+	private String orderer;
+	private String receiver;
+	private String express_company;
+	private boolean checked;
+	private String reg_date;
+	@Override
+	public String toString() {
+		return "ExpressVO [express_id=" + express_id + ", user_id=" + user_id
+				+ ", orderer=" + orderer + ", receiver=" + receiver
+				+ ", express_company=" + express_company + ", checked="
+				+ checked + ", reg_date=" + reg_date + "]";
+	}
+	public int getExpress_id() {
+		return express_id;
+	}
+	public void setExpress_id(int express_id) {
+		this.express_id = express_id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getOrderer() {
+		return orderer;
+	}
+	public void setOrderer(String orderer) {
+		this.orderer = orderer;
+	}
+	public String getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	public String getExpress_company() {
+		return express_company;
+	}
+	public void setExpress_company(String express_company) {
+		this.express_company = express_company;
+	}
+	public boolean isChecked() {
+		return checked;
+	}
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+	public String getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+}
